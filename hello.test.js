892350e02743f6070d4a40dc55b0dd48008556f5
Cheyenne Miller <cheyenne@Cheyennes-MacBook-Air.local>
2024-01-12 20:32:20 -0800

    test 3

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,10 @@
-const hello = require("./hello");
-describe("My hello", () => {
-  test("works", () => {
-    expect(hello.hello()).toEqual("Hello World");
+const request = require('supertest');
+const app = require('./hello');
+
+describe('GET /', () => {
+  it('responds with Hello World', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toContain('Hello World');
   });
 });
